7efb963d861c6641c7ccb8c270ab9166a52443c6
Tyler Hatton <tyler.hatton@gmail.com>
2021-06-19 23:39:19 -0400

    added service catalog item client file

---
+++ servicenow/client/client_service_catalog_item.go
@@ -0,0 +1,19 @@
+package client
+
+// EndpointServiceCatalogItem is the endpoint to manage service catalog item records.
+const EndpointServiceCatalogItem = "sc_cat_item.do"
+
+// ServiceCatalogItem is the json response for a service catalog item in ServiceNow.
+type ServiceCatalogItem struct {
+	BaseResult
+	Name                string `json:"name"`
+	ShortDescription    string `json:"short_description"`
+	Description         string `json:"description"`
+	HideAddToCart       bool   `json:"no_cart,string"`
+	HideQuantity        bool   `json:"no_quantity,string"`
+	HideDeliveryTime    bool   `json:"no_delivery_time_v2,string"`
+	HideAddToWishList   bool   `json:"no_wishlist_v2,string"`
+	HideAttachment      bool   `json:"no_attachment_v2,string"`
+	MandatoryAttachment bool   `json:"mandatory_attachment,string"`
+	Active              bool   `json:"active,string"`
+}
